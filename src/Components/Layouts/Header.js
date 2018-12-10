6fe41faf6e28a1572eb3b346120cffec341f9c73
codepanda26 <codepanda26@live.com>
2018-12-10 14:49:58 +0100

    Updated Header with Material ui

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  MenuIcon,
+  Typography,
+  Button
+} from "material-ui";
+
 //functional component
 export default props => (
-  <div>
-    <h1>Panda Header </h1>
-  </div>
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="headline" color="inherit">
+        Exercise DataBase
+      </Typography>
+    </Toolbar>
+  </AppBar>
 );
